7447a7daf58b303c7716026f19e7c06d780bb85d
Semai-Mohamed <semaimohamed860@gmail.com>
2025-11-21 21:49:08 +0100

    feat: enhance MovieCard layout with star rating and release date display

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,7 +1,7 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity,Image } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
-import { Image } from 'react-native'
+import { icons } from '@/constants/icons'
 
 const MovieCard = ({id,poster_path,title,vote_average,release_date}:Movie) => {
   return (
@@ -9,7 +9,15 @@ const MovieCard = ({id,poster_path,title,vote_average,release_date}:Movie) => {
    <TouchableOpacity className='w-[30%]'>
     <Image source={{uri : poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : undefined}} className='w-full h-52 rounded-lg ' resizeMode='cover'></Image>
     <Text className='text-sm font-bold text-white ' numberOfLines={1}>{title}</Text>
-   </TouchableOpacity>
+    <View className='flex-row items-center justify-start gap-x-1'>
+      <Image source={icons.star} className='size-4'></Image>
+    <Text className='text-xs text-white font-bold uppercase'>{Math.round(vote_average)}</Text>
+    </View>
+    <View className='flex-row items-center justify-between'>
+      <Text className='text-xs text-light-300 font-medium mt-1'>{release_date?.split('-')[0]}</Text>
+      {/* <Text className='text-xs font-medium text-light-300 uppercase'></Text> */}
+    </View>
+       </TouchableOpacity>
    </Link>
   )
 }
